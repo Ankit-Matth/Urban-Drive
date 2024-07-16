b29448b5851d355ced7ff81518e1a61d5314bc70
Ankit-Matth <ankitmatth111@gmail.com>
2024-07-16 14:48:08 +0530

    updating common layout logic

--- src/app/CommonLayout.js
+++ src/app/CommonLayout.js
@@ -1,28 +1,17 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import { useAppSelector } from '@/lib/hooks'
 
 const CommonLayout = ({children}) => {
-    const [showCommonLayout, setShowCommonLayout] = useState(true);
-    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
-    const userRole = useAppSelector((state) => state.role.userRole);
-    const contactCategory = useAppSelector((state) => state.contact.contactCategory);
-
-    useEffect(() => {
-        if (contactCategory) {
-            setShowCommonLayout(false);
-        } else if (userRole !== '' && !isLoggedIn) {
-            setShowCommonLayout(false);
-        } else {
-            setShowCommonLayout(true);
-        }
-    }, [contactCategory,userRole,isLoggedIn]);
+    const isCommonLayoutVisible = useAppSelector((state) => state.layout.isCommonLayoutVisible);
 
     return (
         <>
-            {showCommonLayout && <Header />}
+            {isCommonLayoutVisible && <Header />}
             {children}
+            {isCommonLayoutVisible && <Footer />}
         </>
     );
 }
