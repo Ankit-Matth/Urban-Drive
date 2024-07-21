42eee36034b845e121e2a4a1f5bbab1ded324b7c
Ankit-Matth <ankitmatth111@gmail.com>
2024-07-21 20:45:49 +0530

    updating login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,12 @@
 "use client"
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
-import { useAppSelector, useAppDispatch } from '@/lib/hooks'
-import { login } from '@/lib/features/auth/authSlice'
+import { useAppSelector, useAppDispatch } from '@/lib/hooks';
+import { setRole } from "@/lib/features/role/roleSlice";
+import { showCommonLayout , hideCommonLayout } from "@/lib/features/layout/layoutSlice";
+import { setIsAuthRequired } from "@/lib/features/auth/authSlice";
+import Link from 'next/link';
+import { loginHandler } from '@/actions/login';
 
 const Page = () => {
   const router = useRouter();
@@ -21,39 +25,51 @@ const Page = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    dispatch(login())
+  const loginBtnClick = async (formData) => {
+    let res = await loginHandler(formData)
 
-    if (contactCategory) {
-      router.push(`/contact/?category=${contactCategory}`);
-    }
-    if (isAdmin) {
-      router.push('/admin');
-    }
-    if (userRole === 'rider') {
-      router.push('/riders');
-    }
-    if (userRole === 'driver') {
-      router.push('/drivers');
+    if (res) {
+      dispatch(setIsAuthRequired(false))
+      
+      if (contactCategory) {
+        router.push(`/contact/?category=${contactCategory}`);
+      }
+
+      if (isAdmin) {
+        dispatch(showCommonLayout());
+        router.push('/admin');
+      }
+
+      if (userRole === 'rider') {
+        dispatch(showCommonLayout());
+        router.push('/riders');
+      }
+
+      if (userRole === 'driver') {
+        dispatch(showCommonLayout());
+        router.push('/drivers');
+      }
+
+    } else {
+      console.log("Login failed")
     }
   };
-
+ 
   return (
     <div className='min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4'>
       <div className='bg-white shadow-md rounded-lg w-full max-w-md p-6'>
             <h2 className="text-3xl font-bold text-center text-gray-800 mt-4">
               {isAdmin ? "Admin Page" : "Login Page"}
             </h2>
-            <form onSubmit={handleLogin} className="space-y-4">
+            <form action={loginBtnClick} className="space-y-4">
            <div className='mt-12'>
             <input
               type="text"
               name="email"
-              value={formData.email}
-              placeholder={`${isAdmin ? "admin@urbandrive" : "Enter your email"}`}
+              value={`${isAdmin ? "admin@urbandrive" : formData.email}`}
+              placeholder="Enter your email"
               onChange={handleInputChange}
-              disabled={isAdmin ? true : false}
+              readOnly={isAdmin ? true : false}
               className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 ${isAdmin ? 'hover:cursor-not-allowed' : ''}`}
               required
             />
@@ -69,6 +85,12 @@ const Page = () => {
               required
             />
           </div>
+          {contactCategory && 
+                <div className='flex flex-row justify-between pl-1 pb-2'>
+                    <Link href="/signup" onClick={()=>{dispatch(setRole("rider"))}} className="text-sm text-indigo-600 underline">Not have an account?</Link>
+                    <Link href="/signup" onClick={()=>{dispatch(setRole("driver"))}} className="text-sm text-indigo-600 underline">SignUp as a Driver</Link>
+                </div>
+          }
           <button
             type="submit"
             className="w-full py-2 px-4 bg-indigo-600 text-white font-medium rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
