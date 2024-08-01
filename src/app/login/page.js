f7f8b04458e96737cf569277914b7002c4a8bae1
Ankit-Matth <ankitmatth111@gmail.com>
2024-08-01 20:11:11 +0530

    updating login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -14,6 +14,8 @@ const Page = () => {
   const userRole = useAppSelector((state) => state.role.userRole);
   const contactCategory = useAppSelector((state) => state.contact.contactCategory);
   const isAdmin = userRole === "admin"
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const [formData, setFormData] = useState({
     email: '',
@@ -21,19 +23,17 @@ const Page = () => {
   });
 
   const handleInputChange = (e) => {
+    setError('')
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const loginBtnClick = async (formData) => {
+  const handleLoginBtnClick = async (formData) => {
+    setIsLoading(true)
     let res = await loginHandler(formData)
 
-    if (res) {
+    if (res.success) {
       dispatch(setIsAuthRequired(false))
-      
-      if (contactCategory) {
-        router.push(`/contact/?category=${contactCategory}`);
-      }
 
       if (isAdmin) {
         dispatch(showCommonLayout());
@@ -50,21 +50,29 @@ const Page = () => {
         router.push('/drivers');
       }
 
+      if (contactCategory) {
+        router.push(`/contact/?category=${contactCategory}`);
+      }
+
     } else {
-      console.log("Login failed")
+      setError(res.message)
+      if (res.err) {
+        console.error(res.err)
+      }
+      setIsLoading(false)
     }
   };
  
   return (
     <div className='min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4'>
-      <div className='bg-white shadow-md rounded-lg w-full max-w-md p-6'>
-            <h2 className="text-3xl font-bold text-center text-gray-800 mt-4">
+      <div className='bg-white shadow-md rounded-lg w-full max-w-md p-8'>
+            <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
               {isAdmin ? "Admin Page" : "Login Page"}
             </h2>
-            <form action={loginBtnClick} className="space-y-4">
-           <div className='mt-12'>
+            <form action={handleLoginBtnClick} className="space-y-4">
+           <div>
             <input
-              type="text"
+              type="email"
               name="email"
               value={`${isAdmin ? "admin@urbandrive" : formData.email}`}
               placeholder="Enter your email"
@@ -85,9 +93,13 @@ const Page = () => {
               required
             />
           </div>
+          {error && 
+           <div className="text-red-600 text-center block w-full px-3 py-2 rounded-md bg-red-200">
+            {error}
+           </div>}
           {contactCategory && 
                 <div className='flex flex-row justify-between pl-1 pb-2'>
-                    <Link href="/signup" onClick={()=>{dispatch(setRole("rider"))}} className="text-sm text-indigo-600 underline">Not have an account?</Link>
+                    <Link href="/signup" onClick={()=>{dispatch(setRole("rider"))}} className="text-sm text-indigo-600 underline">SignUp as a Rider</Link>
                     <Link href="/signup" onClick={()=>{dispatch(setRole("driver"))}} className="text-sm text-indigo-600 underline">SignUp as a Driver</Link>
                 </div>
           }
@@ -95,7 +107,11 @@ const Page = () => {
             type="submit"
             className="w-full py-2 px-4 bg-indigo-600 text-white font-medium rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Login
+            {isLoading ? (
+                <div className="loader"></div>
+            ) : (
+              "Login"
+            )}
           </button>
         </form>
       </div>
