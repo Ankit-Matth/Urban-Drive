3d14707d97a238fb62ecf715537babef10178f65
Ankit-Matth <ankitmatth111@gmail.com>
2024-08-01 20:08:55 +0530

    updating actions

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,21 +1,38 @@
 "use server"
 import { signIn } from '@/auth'
+import { compare } from 'bcryptjs'
+import connectDB from '@/utils/dbConfig'
+import UserModel from "@/models/Users"
 
 export const loginHandler = async (formData) => {
     const email = formData.get("email")
     const password = formData.get("password")
 
     try {
-        await signIn("credentials",{
-            email,
-            password,
+        await connectDB()
+
+        const user = await UserModel.findOne({ email })
+  
+        if (!user) {
+          return { success: false, message: "Invalid email or password" };
+        }
+
+        const isPasswordMatched = await compare(password, user.password)
+
+        if (isPasswordMatched) {
+          const { password, ...userWithoutPassword } = user.toObject();
+          
+          await signIn("credentials",{
+            userWithoutPassword,
             redirect: false
-        })
+          })
 
-        return true;
+          return { success: true, message: "Login successful" };
 
-    } catch (err) {
-        console.log("Login failed")
-        return false;
+        } else {
+            return { success: false, message: "Invalid email or password" };
+        }
+    } catch (error) {
+        return { success: false, message: "Something went wrong", err: error.message};
     }
 }
